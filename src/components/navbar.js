8667731ea577dad98154e995faeb766db2f1e926
Ken <103093259+ken862734801@users.noreply.github.com>
2022-08-28 15:42:23 -0400

    Add files via upload

---
+++ src/components/navbar.js
@@ -0,0 +1,22 @@
+export default function navbar(){
+    const content = document.getElementById("content");
+
+    function createNavList(text){
+        const li = document.createElement("li");
+        li.textContent = text;
+        li.id = text;
+    
+        return li;
+    };
+
+    let nav = document.createElement("nav");
+    let navbar = document.createElement("navbar");
+
+    nav.appendChild(navbar);
+    navbar.appendChild(createNavList("Home"));
+    navbar.appendChild(createNavList("Menu"));
+    navbar.appendChild(createNavList("Contact"));
+
+    content.appendChild(nav);
+
+}
