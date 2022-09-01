eacd8c090245d52f8771779d98197e880c341453
Ken <103093259+ken862734801@users.noreply.github.com>
2022-09-01 13:10:58 -0400

    Update home.js

--- src/home.js
+++ src/home.js
@@ -1,90 +1,121 @@
-export default function loadHome(){
+import {heroNavbar} from "./load";
+import {createHeader2 } from "./load";
+import { createHeader3 } from "./load";
+import {createParagraph} from "./load";
+import { createHeader5 } from "./load";
+import { createListElements } from "./load";
+
+export function loadHome(){
 
     //Variable for content div.
     const content = document.getElementById("content");
 
     //Variables for header section.
-    let header = document.createElement("section");
-    let nav = document.createElement("nav");
-    let navbar = document.createElement("navbar");
+    let hero = document.createElement("section");
+    
+
     let logo = document.createElement("img");
 
-    header.className = "header";
-    header.appendChild(nav);
-    header.appendChild(logo);
+    hero.className = "hero-section";
+    hero.id = "hero-section";
+    logo.src = "../dist/images/logo.png";
 
-    nav.appendChild(navbar);
-    navbar.appendChild(createNavList("Home"));
-    navbar.appendChild(createNavList("Menu"));
-    navbar.appendChild(createNavList("Contact"));
-    logo.src = "./images/logo.png";
+    content.appendChild(hero);
+    
+    heroNavbar();
+    hero.appendChild(logo);
 
     //Variables for about section.
     let about = document.createElement("section");
-    about.className = "about";
-    about.appendChild(createHeader2("Buon cibo. Buon vino. Buon amici."));
-    about.appendChild(createParagraph("Good food. Good wine. Good friends. The atmosphere of Italy dictates the mood at Brocelle. We offer you not only the best italian dishes, but also the ambiance, interior, and music that willtransform an average night to one under the stars of Italy."));
+
+    about.className = "about-section";
+    about.id = "about-section";
+    about.appendChild(createHeader2('"Buon cibo. Buon vino. Buon amici."'));
+    about.appendChild(createParagraph("Good food. Good wine. Good friends. The atmosphere of Italy dictates the mood at Brocelle. We offer you not only the best italian dishes, but also the ambiance, interior, and music that will transform an average night to one under the stars of Italy."));
+    content.appendChild(about);
 
     //Variables for menu section.
     let menu = document.createElement("section");
-    let imageContainer = document.createElement("div");
-    let textContainer = document.createElement("div");
-    let image = document.createElement("img");
-    let btnContainer = document.createElement("div");
-
-    imageContainer.className = "image-container";
-    imageContainer.appendChild(image);
-    image.src="./images/pasta.png";
-
-    textContainer.className = "text-container";
-    textContainer.appendChild(createParagraph("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque est sed nulla placerat volutpat.Proin non lorem quis arcu rutrum volutpat ut id orci.Mauris sodales hendrerit purus vel iaculis.Proin volutpat ullamcorper sapien, et tempor augue luctus et.Duis condimentum rhoncus elementum. Aliquam sit amet ultricies diam. Nullam in mi neque. Nam quis nibh viverra, malesuada massa non, facilisis augue."))
-
-    btnContainer.className = "btn-container";
-    btnContainer.textContent = "FULL MENU &#10230;"
-
     menu.className = "menu-section";
-    menu.appendChild(textContainer);
-    menu.appendChild(imageContainer);
+    menu.id = "menu-section";
+    
+    let imageContainer = document.createElement("div");
+    imageContainer.className = "image-container";
 
-    textContainer.appendChild("btn-container");
+    let image = document.createElement("img");
+    image.src = "../dist/images/pasta.png";
+
+    imageContainer.appendChild(image);
+
+    let textContainer = document.createElement("div");
+    textContainer.className = "text-container";
+    textContainer.appendChild(createHeader3("MENU"));
+    textContainer.appendChild(createParagraph("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque est sed nulla placerat volutpat. Proin non lorem quis arcu rutrum volutpat ut id orci. Mauris sodales hendrerit purus vel iaculis. Proin volutpat ullamcorper sapien, et tempor augue luctus et. Duis condimentum rhoncus elementum."));
+    textContainer.appendChild(createParagraph("Aliquam sit amet ultricies diam. Nullam in mi neque. Nam quis nibh viverra, malesuada massa non, facilisis augue."));
+
+    let btnContainer = document.createElement("div");
+    btnContainer.className = "btn-container";
+    btnContainer.appendChild(createParagraph("FULL MENU"));
+
+    textContainer.appendChild(btnContainer);
+    
+    menu.appendChild(imageContainer);
+    menu.appendChild(textContainer);
+    content.appendChild(menu);
 
     //Variables for reservation section.
     let reservation = document.createElement("section");
-    let imageContainer2 = document.createElement("div");
-    let textContainer2 = document.createElement("div");
-    let image2 = document.createElement("img");
-    let btnContainer2 = document.createElement("div");
-
-    imageContainer2.className = "image-container";
-    imageContainer2.appendChild(image2);
-    image2.src="./images/collage.png";
-
-    textContainer2.className = "text-container";
-    textContainer2.appendChild(createParagraph("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque est sed nulla placerat volutpat.Proin non lorem quis arcu rutrum volutpat ut id orci.Mauris sodales hendrerit purus vel iaculis.Proin volutpat ullamcorper sapien, et tempor augue luctus et.Duis condimentum rhoncus elementum. Aliquam sit amet ultricies diam. Nullam in mi neque. Nam quis nibh viverra, malesuada massa non, facilisis augue."))
-
-    btnContainer2.className = "btn-container";
-    btnContainer2.textContent = "LEARN MORE &#10230;"
-
     reservation.className = "reservation-section";
-    reservation.appendChild(imageContainer2);
+    reservation.id = "reservation-section";
+
+    let imageContainer2 = document.createElement("div");
+    imageContainer2.className = "image-container";
+
+    let image2 = document.createElement("img");
+    image2.src = "../dist/images/collage.png";
+
+    imageContainer2.appendChild(image2);
+
+    let textContainer2 = document.createElement("div");
+    textContainer2.className = "text-container";
+    textContainer2.appendChild(createHeader3("MAKE A RESERVATION"));
+    textContainer2.appendChild(createParagraph("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque est sed nulla placerat volutpat. Proin non lorem quis arcu rutrum volutpat ut id orci. Mauris sodales hendrerit purus vel iaculis. Proin volutpat ullamcorper sapien, et tempor augue luctus et. Duis condimentum rhoncus elementum."));
+    textContainer2.appendChild(createParagraph("Aliquam sit amet ultricies diam. Nullam in mi neque. Nam quis nibh viverra, malesuada massa non, facilisis augue."));
+
+    let btnContainer2 = document.createElement("div");
+    btnContainer2.className = "btn-container";
+    btnContainer2.appendChild(createParagraph("LEARN MORE"));
+
+    textContainer2.appendChild(btnContainer2);
+
+
     reservation.appendChild(textContainer2);
+    reservation.appendChild(imageContainer2);
+
+    content.appendChild(reservation);
 
     //Variables for information section.
-    let info = document.createElement("section");
-    let locationContainer = document.createElement("div");
-    let hoursContainer = document.createElement("div");
-    let number = document.createElement("p");
-    number.textContent = "202-555-0170";
-    number.className = "number";
+    let information = document.createElement("div");
+    information.className = "information-section";
+    information.id = "information-section";
 
+    let locationContainer = document.createElement("div");
     locationContainer.className = "location-container";
+
+    let number = document.createElement("p");
+    number.className="number";
+    number.textContent = "202-555-0170";
+
+    let hoursContainer = document.createElement("div");
+    hoursContainer.className = "hours-container";
+
+
     locationContainer.appendChild(createHeader5("WHERE TO FIND US"));
     locationContainer.appendChild(createParagraph("125 Timber Ridge Road, Alexandria, CA 12345, USA"));
 
     locationContainer.appendChild(createHeader5("RESERVATIONS"));
     locationContainer.appendChild(number);
 
-    hoursContainer.className = "hours-container";
     hoursContainer.appendChild(createHeader5("HOURS OF OPERATION"));
     hoursContainer.appendChild(createListElements("Monday: 11am - 9pm"));
     hoursContainer.appendChild(createListElements("Tuesday: 11am - 9pm"));
@@ -94,51 +125,9 @@ export default function loadHome(){
     hoursContainer.appendChild(createListElements("Saturday: 5pm - 11pm"));
     hoursContainer.appendChild(createListElements("Sunday: Closed"));
 
-    info.className = "information";
-    info.appendChild(locationContainer);
-    info.appendChild(hoursContainer);
+    information.appendChild(locationContainer);
+    information.appendChild(hoursContainer);
 
-    //Append elements to the DOM.
-    content.appendChild(header);
-    content.appendChild(about);
-    content.appendChild(menu);
-    content.appendChild(reservation);
-    content.appendChild(info);
-};
+    content.appendChild(information);
+}
 
-//Function to create DOM text elements.
-
-function createParagraph(text){
-    const paragraph = document.createElement("p");
-    paragraph.textContent = text;
-
-    return paragraph;
-};
-
-function createHeader2(text){
-    const H2 = document.createElement("h2");
-    H2.textContent = text;
-
-    return H2;
-};
-
-function createHeader5(text){
-    const H5 = document.createElement("h5");
-    H5.textContent = text;
-
-    return H5;
-};
-
-function createNavList(text){
-    const li = document.createElement("li");
-    li.textContent = text;
-    li.id = text;
-
-    return li;
-};
-function createListElements(text){
-    const li = document.createElement("li");
-    li.textContent = text;
-
-    return li;
-};
