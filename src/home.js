97484ab88d60df180cb9e4667616b72e64f9bd9c
Ken <103093259+ken862734801@users.noreply.github.com>
2022-09-03 18:22:52 -0400

    Update home.js

--- src/home.js
+++ src/home.js
@@ -1,10 +1,11 @@
 //Imported functions.
 import {homeNavbar} from "../src/components/homeNavbar";
 import {createHeader2} from "../src/components/DOM";
-import {createHeader3} from "../src/components/DOM";
 import {createParagraph} from "../src/components/DOM";
 import {createHeader5} from "../src/components/DOM";
 import {createListElements} from "../src/components/DOM";
+import {menuSection} from "../src/components/section";
+import {reservationSection} from "../src/components/section";
 
 export function loadHome(){
 
@@ -35,71 +36,11 @@ export function loadHome(){
     about.appendChild(createParagraph("Good food. Good wine. Good friends. The atmosphere of Italy dictates the mood at Brocelle. We offer you not only the best italian dishes, but also the ambiance, interior, and music that will transform an average night to one under the stars of Italy."));
     content.appendChild(about);
 
-    //Variables for menu section.
-    let menu = document.createElement("section");
-    let menuWrapper = document.createElement("div");
-    menuWrapper.className="menu-wrapper";
-    menu.className = "menu-section";
-    menu.id = "menu-section";
-    
-    let imageContainer = document.createElement("div");
-    imageContainer.className = "image-container";
-
-    let image = document.createElement("img");
-    image.src = "../dist/images/pasta.png";
-
-    imageContainer.appendChild(image);
-
-    let textContainer = document.createElement("div");
-    textContainer.className = "text-container";
-    textContainer.appendChild(createHeader3("MENU"));
-    textContainer.appendChild(createParagraph("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque est sed nulla placerat volutpat. Proin non lorem quis arcu rutrum volutpat ut id orci. Mauris sodales hendrerit purus vel iaculis. Proin volutpat ullamcorper sapien, et tempor augue luctus et. Duis condimentum rhoncus elementum."));
-    textContainer.appendChild(createParagraph("Aliquam sit amet ultricies diam. Nullam in mi neque. Nam quis nibh viverra, malesuada massa non, facilisis augue."));
-
-    let btnContainer = document.createElement("div");
-    btnContainer.className = "btn-container";
-    btnContainer.id = "menu-arrow";
-    btnContainer.innerHTML = "FULL MENU &#10230;"
-
-    textContainer.appendChild(btnContainer);
-    
-    menuWrapper.appendChild(imageContainer);
-    menuWrapper.appendChild(textContainer);
-
-    menu.appendChild(menuWrapper);
-    content.appendChild(menu);
+    //Function for menu section.
+    menuSection();
 
     //Variables for reservation section.
-    let reservation = document.createElement("section");
-    reservation.className = "reservation-section";
-    reservation.id = "reservation-section";
-
-    let imageContainer2 = document.createElement("div");
-    imageContainer2.className = "image-container";
-
-    let image2 = document.createElement("img");
-    image2.src = "../dist/images/collage.png";
-
-    imageContainer2.appendChild(image2);
-
-    let textContainer2 = document.createElement("div");
-    textContainer2.className = "text-container";
-    textContainer2.appendChild(createHeader3("MAKE A RESERVATION"));
-    textContainer2.appendChild(createParagraph("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque est sed nulla placerat volutpat. Proin non lorem quis arcu rutrum volutpat ut id orci. Mauris sodales hendrerit purus vel iaculis. Proin volutpat ullamcorper sapien, et tempor augue luctus et. Duis condimentum rhoncus elementum."));
-    textContainer2.appendChild(createParagraph("Aliquam sit amet ultricies diam. Nullam in mi neque. Nam quis nibh viverra, malesuada massa non, facilisis augue."));
-
-    let btnContainer2 = document.createElement("div");
-    btnContainer2.className = "btn-container";
-    btnContainer2.id = "reservation-arrow";
-    btnContainer2.innerHTML = "LEARN MORE &#10230;"
-
-    textContainer2.appendChild(btnContainer2);
-
-
-    reservation.appendChild(textContainer2);
-    reservation.appendChild(imageContainer2);
-
-    content.appendChild(reservation);
+    reservationSection();
 
     //Variables for information section.
     let information = document.createElement("div");
@@ -137,5 +78,9 @@ export function loadHome(){
 
     content.appendChild(information);
 };
+
+
+
+
 
 
